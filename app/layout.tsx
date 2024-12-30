1a921a4e40d3a0da271c2ea0762574c4db92bd25
Abby <i@abby.md>
2024-12-31 02:58:40 +0900

    refactor(layout): simplify font handling and update favicon import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import favicon from "./favicon.png";
+import favicon from "../public/favicon.png";
 import { SettingsProvider } from './settings-context'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "GTSDB Admin",
@@ -31,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <SettingsProvider>
           {children}
